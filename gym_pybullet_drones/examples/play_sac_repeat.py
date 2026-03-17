c7da412c8f7ce45e59352834690c640b572aad0d
Luhar2662 <rahul.kalavagunta@gmail.com>
2026-03-17 17:24:27 -0400

    added shallow eval and demo scripts

--- gym_pybullet_drones/examples/play_sac_repeat.py
+++ gym_pybullet_drones/examples/play_sac_repeat.py
@@ -16,6 +16,7 @@ from gym_pybullet_drones.utils.utils import sync, str2bool
 
 DEFAULT_MODEL_PATH = "results/best_model.zip"
 DEFAULT_RANDOM_INIT = True
+DEFAULT_NUM_RUNS = 5
 DEFAULT_RANDOM_EVAL = False
 DEFAULT_SAFE_INIT = False
 DEFAULT_GUI = True
@@ -96,7 +97,7 @@ def random_target():
     
     return random_target, random_target_rpy
 
-def play(model_path=DEFAULT_MODEL_PATH, multiagent=DEFAULT_MA, gui=DEFAULT_GUI, random_init = DEFAULT_RANDOM_INIT, random_eval = DEFAULT_RANDOM_EVAL, safe_init = DEFAULT_SAFE_INIT, record = True):
+def play(model_path=DEFAULT_MODEL_PATH, multiagent=DEFAULT_MA, gui=DEFAULT_GUI, random_init = DEFAULT_RANDOM_INIT, random_eval = DEFAULT_RANDOM_EVAL, safe_init = DEFAULT_SAFE_INIT, record = True, num_runs = DEFAULT_NUM_RUNS):
     #### Load saved model ####
     if not os.path.isfile(model_path):
         print(f"[ERROR] Model file not found at: {model_path}")
@@ -146,63 +147,82 @@ def play(model_path=DEFAULT_MODEL_PATH, multiagent=DEFAULT_MA, gui=DEFAULT_GUI, 
                     num_drones=DEFAULT_AGENTS if multiagent else 1,
                     output_folder="logs_playback/",
                     colab=False)
+    time.sleep(3)
+    for _ in range(num_runs):
+        #### Run the simulation ####
+        obs, _ = env.reset(seed=42, options={})
+        
+        print("random start point: ", obs)
+        start = time.time()
 
-    #### Run the simulation ####
-    obs, _ = env.reset(seed=42, options={})
-    
-    print("random start point: ", obs)
-    start = time.time()
+        for i in range((env.EPISODE_LEN_SEC-1)*env.CTRL_FREQ):
+            action, _ = model.predict(obs, deterministic=True)
+            obs, reward, terminated, truncated, info = env.step(action)
 
-    for i in range((env.EPISODE_LEN_SEC+2)*env.CTRL_FREQ):
-        action, _ = model.predict(obs, deterministic=True)
-        obs, reward, terminated, truncated, info = env.step(action)
+            obs2 = obs.squeeze()
+            act2 = action.squeeze()
 
-        obs2 = obs.squeeze()
-        act2 = action.squeeze()
-
-        if DEFAULT_OBS == ObservationType.KIN:
-            if not multiagent:
-                logger.log(drone=0,
-                    timestamp=i/env.CTRL_FREQ,
-                    state=np.hstack([obs2[0:3],
-                                     np.zeros(4),
-                                     obs2[3:15],
-                                     act2]),
-                    control=np.zeros(12))
-            else:
-                for d in range(DEFAULT_AGENTS):
-                    logger.log(drone=d,
+            if DEFAULT_OBS == ObservationType.KIN:
+                if not multiagent:
+                    logger.log(drone=0,
                         timestamp=i/env.CTRL_FREQ,
-                        state=np.hstack([obs2[d][0:3],
-                                         np.zeros(4),
-                                         obs2[d][3:15],
-                                         act2[d]]),
+                        state=np.hstack([obs2[0:3],
+                                        np.zeros(4),
+                                        obs2[3:15],
+                                        act2]),
                         control=np.zeros(12))
-                    
-        #Track Safety Violations Here!
-        ground_margin = obs2[2]
-        left_margin = 1 - obs2[1]
-        right_margin = obs2[1] + 1
-        front_margin = 1 - obs2[0]
-        back_margin = obs2[0] + 1
-        ceil_margin = 2 - obs2[2]
-        safety_margin = min(ground_margin, ceil_margin, back_margin, front_margin, left_margin, right_margin)
-        if safety_margin <= 0:
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            print("Safety violation: state is [",obs2[0],", ", obs2[1],", ", obs2[2],"]")
-            print("pausing for 2 seconds")
-            time.sleep(2)
-        obs_t, _ = model.policy.obs_to_tensor(obs)
-        act_t = th.as_tensor(action, device=model.device).float()
-        print("qnet average predicts: ", critic_check(obs_t, act_t), ", for obs: ", obs2[0:3], ", act: ", act2)
+                else:
+                    for d in range(DEFAULT_AGENTS):
+                        logger.log(drone=d,
+                            timestamp=i/env.CTRL_FREQ,
+                            state=np.hstack([obs2[d][0:3],
+                                            np.zeros(4),
+                                            obs2[d][3:15],
+                                            act2[d]]),
+                            control=np.zeros(12))
+                        
+            #Track Safety Violations Here!
+            ground_margin = obs2[2]
+            left_margin = 1 - obs2[1]
+            right_margin = obs2[1] + 1
+            front_margin = 1 - obs2[0]
+            back_margin = obs2[0] + 1
+            ceil_margin = 2 - obs2[2]
+            safety_margin = min(ground_margin, ceil_margin, back_margin, front_margin, left_margin, right_margin)
+            if safety_margin <= 0:
+                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                print("Safety violation: state is [",obs2[0],", ", obs2[1],", ", obs2[2],"]")
+                print("pausing for 2 seconds")
+                time.sleep(2)
+            obs_t, _ = model.policy.obs_to_tensor(obs)
+            act_t = th.as_tensor(action, device=model.device).float()
+            print("qnet average predicts: ", critic_check(obs_t, act_t), ", for obs: ", obs2[0:3], ", act: ", act2)
 
-        env.render()
-        sync(i, start, env.CTRL_TIMESTEP)
-        if terminated:
-            break
+            env.render()
+            sync(i, start, env.CTRL_TIMESTEP)
+            if terminated:
+                break
 
+        #RESET LOOP!
+        if random_init:
+            if random_eval:
+                start_pos, start_rpy = eval_random_target()
+            elif safe_init:
+                start_pos, start_rpy = safe_random_target(model)
+            else:
+                start_pos, start_rpy = random_target()
+        else:
+            start_pos = np.array([[0,
+                                0,
+                                .1] for i in range(1)])
+            start_rpy = np.array([[0,
+                            0,
+                            0,
+                            ] for i in range(1)])
+        env.INIT_XYZS = start_pos
+        env.INIT_RPYS = start_rpy
     env.close()
-    logger.plot()
+    #logger.plot()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run a trained PPO policy in PyBullet drones environment.")
@@ -212,6 +232,7 @@ if __name__ == "__main__":
     parser.add_argument('--random_init', default=DEFAULT_RANDOM_INIT, type=str2bool,           help='whether to random init during rollout', metavar='')
     parser.add_argument('--random_eval', default=DEFAULT_RANDOM_EVAL, type=str2bool,           help='whether to randomize according to the eval bounds set in eval_random_target()', metavar='')
     parser.add_argument('--safe_init', default=DEFAULT_SAFE_INIT, type=str2bool,           help='whether to randomize according to a safety check', metavar='')
+    parser.add_argument('--num_runs', default=DEFAULT_NUM_RUNS, type=int,           help='how many times to random init and play out sim', metavar='')
     
     args = parser.parse_args()
 
--- gym_pybullet_drones/examples/shallow_eval.py
+++ gym_pybullet_drones/examples/shallow_eval.py
@@ -16,9 +16,10 @@ from gym_pybullet_drones.utils.utils import sync, str2bool
 
 DEFAULT_MODEL_PATH = "results/best_model.zip"
 DEFAULT_RANDOM_INIT = True
-DEFAULT_RANDOM_EVAL = False
+DEFAULT_NUM_RUNS = 5
+DEFAULT_RANDOM_EVAL = True
 DEFAULT_SAFE_INIT = False
-DEFAULT_GUI = True
+DEFAULT_GUI = False
 DEFAULT_OBS = ObservationType('kin')
 DEFAULT_ACT = ActionType('rpm')
 DEFAULT_AGENTS = 1
@@ -30,7 +31,7 @@ def eval_random_target():
         Pull from a specific safety margin with certainty, such that we always initialize from meaningful
         positions for testing!
         '''
-        print("called eval random")
+        #print("called eval random")
         intervals = [[-.9,-.5],[.5,.9]]
         z_intervals = [[.1,.5],[1.5,1.9]]
 
@@ -96,7 +97,8 @@ def random_target():
     
     return random_target, random_target_rpy
 
-def play(model_path=DEFAULT_MODEL_PATH, multiagent=DEFAULT_MA, gui=DEFAULT_GUI, random_init = DEFAULT_RANDOM_INIT, random_eval = DEFAULT_RANDOM_EVAL, safe_init = DEFAULT_SAFE_INIT, record = True):
+def play(model_path=DEFAULT_MODEL_PATH, multiagent=DEFAULT_MA, gui=DEFAULT_GUI, random_init = DEFAULT_RANDOM_INIT, random_eval = DEFAULT_RANDOM_EVAL, safe_init = DEFAULT_SAFE_INIT, record = True, num_runs = DEFAULT_NUM_RUNS):
+   
     #### Load saved model ####
     if not os.path.isfile(model_path):
         print(f"[ERROR] Model file not found at: {model_path}")
@@ -117,8 +119,6 @@ def play(model_path=DEFAULT_MODEL_PATH, multiagent=DEFAULT_MA, gui=DEFAULT_GUI, 
     if random_init:
         if random_eval:
             start_pos, start_rpy = eval_random_target()
-        elif safe_init:
-            start_pos, start_rpy = safe_random_target(model)
         else:
             start_pos, start_rpy = random_target()
     else:
@@ -147,62 +147,49 @@ def play(model_path=DEFAULT_MODEL_PATH, multiagent=DEFAULT_MA, gui=DEFAULT_GUI, 
                     output_folder="logs_playback/",
                     colab=False)
 
-    #### Run the simulation ####
-    obs, _ = env.reset(seed=42, options={})
-    
-    print("random start point: ", obs)
-    start = time.time()
 
-    for i in range((env.EPISODE_LEN_SEC+2)*env.CTRL_FREQ):
+    unsafe_samples = 0
+    unsafe_list = []
+
+    for _ in range(num_runs):
+        #### Run the simulation ####
+        obs, _ = env.reset(seed=42, options={})
         action, _ = model.predict(obs, deterministic=True)
-        obs, reward, terminated, truncated, info = env.step(action)
-
-        obs2 = obs.squeeze()
-        act2 = action.squeeze()
-
-        if DEFAULT_OBS == ObservationType.KIN:
-            if not multiagent:
-                logger.log(drone=0,
-                    timestamp=i/env.CTRL_FREQ,
-                    state=np.hstack([obs2[0:3],
-                                     np.zeros(4),
-                                     obs2[3:15],
-                                     act2]),
-                    control=np.zeros(12))
-            else:
-                for d in range(DEFAULT_AGENTS):
-                    logger.log(drone=d,
-                        timestamp=i/env.CTRL_FREQ,
-                        state=np.hstack([obs2[d][0:3],
-                                         np.zeros(4),
-                                         obs2[d][3:15],
-                                         act2[d]]),
-                        control=np.zeros(12))
-                    
-        #Track Safety Violations Here!
-        ground_margin = obs2[2]
-        left_margin = 1 - obs2[1]
-        right_margin = obs2[1] + 1
-        front_margin = 1 - obs2[0]
-        back_margin = obs2[0] + 1
-        ceil_margin = 2 - obs2[2]
-        safety_margin = min(ground_margin, ceil_margin, back_margin, front_margin, left_margin, right_margin)
-        if safety_margin <= 0:
-            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-            print("Safety violation: state is [",obs2[0],", ", obs2[1],", ", obs2[2],"]")
-            print("pausing for 2 seconds")
-            time.sleep(2)
+        
         obs_t, _ = model.policy.obs_to_tensor(obs)
         act_t = th.as_tensor(action, device=model.device).float()
-        print("qnet average predicts: ", critic_check(obs_t, act_t), ", for obs: ", obs2[0:3], ", act: ", act2)
 
-        env.render()
-        sync(i, start, env.CTRL_TIMESTEP)
-        if terminated:
-            break
+        with th.no_grad():
+            q1, q2 = model.policy.critic(obs_t, act_t)
 
+        q_avg = np.mean([q1.item(), q2.item()])
+
+        if q_avg < 0:
+            unsafe_samples += 1
+            unsafe_list.append([env.INIT_XYZS,env.INIT_RPYS])
+        
+
+        #RESET LOOP!
+        if random_init:
+            if random_eval:
+                start_pos, start_rpy = eval_random_target()
+            else:
+                start_pos, start_rpy = random_target()
+        else:
+            start_pos = np.array([[0,
+                                0,
+                                .1] for i in range(1)])
+            start_rpy = np.array([[0,
+                            0,
+                            0,
+                            ] for i in range(1)])
+        env.INIT_XYZS = start_pos
+        env.INIT_RPYS = start_rpy
     env.close()
-    logger.plot()
+
+    success_rate = float(num_runs - unsafe_samples) / float(num_runs)
+    print("Rate of success: ", success_rate)
+    #logger.plot()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run a trained PPO policy in PyBullet drones environment.")
@@ -211,7 +198,8 @@ if __name__ == "__main__":
     parser.add_argument('--gui', type=bool, default=DEFAULT_GUI, help='Enable GUI rendering')
     parser.add_argument('--random_init', default=DEFAULT_RANDOM_INIT, type=str2bool,           help='whether to random init during rollout', metavar='')
     parser.add_argument('--random_eval', default=DEFAULT_RANDOM_EVAL, type=str2bool,           help='whether to randomize according to the eval bounds set in eval_random_target()', metavar='')
-    parser.add_argument('--safe_init', default=DEFAULT_SAFE_INIT, type=str2bool,           help='whether to randomize according to a safety check', metavar='')
+    
+    parser.add_argument('--num_runs', default=DEFAULT_NUM_RUNS, type=int,           help='how many times to random init and play out sim', metavar='')
     
     args = parser.parse_args()
 
